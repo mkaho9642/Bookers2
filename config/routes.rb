0a4c1c96425c6c44ae5f3c4b0272b1ab0d4a3d35
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-03-30 18:17:59 +0000

    [Update] about,login/logout_url/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
+  get 'home/about', to: 'homes#about', as: :about
   resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
