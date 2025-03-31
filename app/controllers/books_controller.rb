7bb0e3f9f35c9ab77fb17ade336c7de2172699de
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-03-31 13:17:26 +0000

    [Update] view/controller etc..

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/post_images'
+  end
+
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
