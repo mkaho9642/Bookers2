655747ec53f2dbbaa0fb049a46a68d827494eda5
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-04-05 10:16:50 +0000

    [Update] view/controller etc..

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    if @book.save!
+    if @book.save
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
@@ -48,7 +48,6 @@ class BooksController < ApplicationController
   end
 
   private
-
   def book_params
     params.require(:book).permit(:title, :body)
   end
